dfded2fa2590e5ff95206105e44aa6c3e635f381
barciewicz <brt666@poczta.onet.pl>
2020-01-23 21:45:02 +0100

    random colored pads

--- sampler/static/sampler/sampler.js
+++ sampler/static/sampler/sampler.js
@@ -120,7 +120,7 @@ class Waveform {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     }
     
-    drawWave() {             
+    drawWave() {            
         let samples = this.sample_data.downloaded_sample_samples
         let abs_max = this.sample_data.downloaded_sample_abs_max
     
