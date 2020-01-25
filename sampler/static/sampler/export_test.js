9574e0b59450ad846e94b9e81e0343fa519d844a
barciewicz <brt666@poczta.onet.pl>
2020-01-25 11:15:45 +0100

    Refactoring static assets

--- sampler/static/sampler/export_test.js
+++
@@ -1,9 +0,0 @@
-export {Test}
-
-class Test {
-    constructor() {
-        this.test = 'test'
-    }
-}
-
-console.log('gdg')
--- sampler/static/sampler/export_test2.js
+++
@@ -1,5 +0,0 @@
-import {Test} from './export_test.js'
-
-let test = new Test()
-console.log(test)
-console.log('dgdgdg')
