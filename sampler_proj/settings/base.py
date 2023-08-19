d7d9a1a2fc3224a7f4a7f6a3856357312b5e7527
bartwroblewski <barti.wroblewski@gmail.com>
2023-08-19 17:52:49 +0200

    fixed migrate

--- sampler_proj/settings/base.py
+++ sampler_proj/settings/base.py
@@ -55,18 +55,13 @@ WSGI_APPLICATION = 'sampler_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
