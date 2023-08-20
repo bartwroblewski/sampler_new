d69fdbbaa2e56d251d1014af7605c4cc0ad950ed
bartwroblewski <barti.wroblewski@gmail.com>
2023-08-20 16:57:43 +0200

    allowed hosts

--- sampler_proj/settings/local.py
+++ sampler_proj/settings/local.py
@@ -3,6 +3,6 @@ from .base import *
 DEBUG = True
 
 # should be empty, providing IP just to allow running in dev mode on Digital Ocean
-ALLOWED_HOSTS = ['46.101.156.79:8002']
+ALLOWED_HOSTS = ['46.101.156.79']
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
